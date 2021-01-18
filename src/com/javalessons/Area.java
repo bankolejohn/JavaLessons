1886743f06f7d325d1a6ba11fd673ef4b64978fa
bankolejohn <bankolejohn@gmail.com>
2021-01-18 01:11:39 +0100

    area of a circle

---
+++ src/com/javalessons/Area.java
@@ -0,0 +1,12 @@
+package com.javalessons;
+// to show the area of a circle
+public class Area {
+    public static void main(String[] args) {
+        double pi, r, a;
+        r = 10.8;
+        pi = 3.1416;
+
+        a = pi*r*r;
+        System.out.println("The area is " + a);
+    }
+}
