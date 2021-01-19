543411b91ca1b9695da547a1a57af0319c992885
bankolejohn <bankolejohn@gmail.com>
2021-01-19 01:22:57 +0100

    compute distance light travels using long variables

---
+++ src/com/javalessons/light.java
@@ -0,0 +1,19 @@
+package com.javalessons;
+
+public class light {
+    public static void main(String args[]) {
+        int lightspeed;
+        long days;
+        long seconds;
+        long distance;
+
+        lightspeed = 18600;
+        days = 1000;
+        seconds = days * 24 * 60 * 60;
+        distance = lightspeed * seconds;
+
+        System.out.print("in " + days);
+        System.out.print("days light will travel about ");
+        System.out.println( distance + " miles");
+    }
+}
