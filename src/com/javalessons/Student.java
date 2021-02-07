a64730659ac90674c00edf77fdfb5b05883a16fe
bankolejohn <bankolejohn@gmail.com>
2021-02-08 00:14:31 +0100

    assignment 1

---
+++ src/com/javalessons/Student.java
@@ -0,0 +1,19 @@
+package com.javalessons;
+
+public class Student {
+    int id;
+    String name;
+    int age;
+}
+
+class
+
+    public static void main (String args[]) {
+    Student mark = new Student();
+    mark.id = 1;
+    mark.name = "Mark";
+    mark.age = 15;
+
+    System.out.println(mark.name + "is" + mark.age + " years old");
+
+    }
